ff0e95aa97852f93fae1536c6c189713b4faef25
Elena Muratova <ysmur@yandex.ru>
2022-12-26 19:25:23 +0300

    добавлен фейерверк

--- game.py
+++ game.py
@@ -1,7 +1,38 @@
 import os
+import random
 import sys
 import pygame
+GRAVITY = 0.1
 
+
+class Particle(pygame.sprite.Sprite):
+    def __init__(self, app, pos, dx, dy):
+        super().__init__(app.all_sprites)
+        # сгенерируем частицы разного размера
+        self.fire = [app.load_image("star.png")]
+        for scale in (5, 10, 20):
+            self.fire.append(pygame.transform.scale(self.fire[0], (scale, scale)))
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+
+        # у каждой частицы своя скорость — это вектор
+        self.velocity = [dx, dy]
+        # и свои координаты
+        self.rect.x, self.rect.y = pos
+
+        # гравитация будет одинаковой (значение константы)
+        self.gravity = GRAVITY
+
+    def update(self):
+        # применяем гравитационный эффект:
+        # движение с ускорением под действием гравитации
+        self.velocity[1] += self.gravity
+        # перемещаем частицу
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        # убиваем, если частица ушла за экран
+        if not self.rect.colliderect((0, 0, app.width, app.height)):
+            self.kill()
 
 class App:
     def __init__(self):
@@ -11,6 +42,7 @@ class App:
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption('Mario')
         self.fps = 50
+        self.all_sprites = pygame.sprite.Group()
 
     def terminate(self):
         pygame.quit()
@@ -32,16 +64,30 @@ class App:
             image = image.convert_alpha()
         return image
 
+    def create_particles(self, position):
+        # количество создаваемых частиц
+        particle_count = 100
+        # возможные скорости
+        numbers = range(-10, 11)
+        for _ in range(particle_count):
+            Particle(self, position, random.choice(numbers), random.choice(numbers))
+
     def run_game(self):
         run = True
         while run:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.terminate()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # создаём частицы по щелчку мыши
+
+                    self.create_particles(pygame.mouse.get_pos())
             # update
 
             # render
             self.screen.fill(pygame.Color('blue'))
+            self.all_sprites.draw(self.screen)
+            self.all_sprites.update()
             pygame.display.flip()
             self.clock.tick(self.fps)
 

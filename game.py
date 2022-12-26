d13709f8f84e7832f4e2f8847236921e27354b4b
murlenka <gym625.ysmur@yandex.ru>
2022-12-26 19:30:27 +0300

    добавлена анимация

--- game.py
+++ game.py
@@ -2,6 +2,28 @@ import os
 import sys
 import pygame
 
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, app, sheet, columns, rows, x, y):
+        super().__init__(app.all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
 
 class App:
     def __init__(self):
@@ -11,6 +33,8 @@ class App:
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption('Mario')
         self.fps = 50
+        self.all_sprites = pygame.sprite.Group()
+        self.dragon = AnimatedSprite(self, self.load_image("dra.png"), 8, 2, 50, 50)
 
     def terminate(self):
         pygame.quit()
@@ -42,6 +66,8 @@ class App:
 
             # render
             self.screen.fill(pygame.Color('blue'))
+            self.all_sprites.draw(self.screen)
+            self.all_sprites.update()
             pygame.display.flip()
             self.clock.tick(self.fps)
 
